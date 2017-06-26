22df112876e9f8a38effc0b2cbf6425d7f6c7bc9
marroq <dffigueroa@galileo.edu>
2017-06-25 22:50:02 -0600

    Niveles del juego

--- Sources/main.c
+++ Sources/main.c
@@ -4,7 +4,9 @@
 #include "stdlib.h"
 
 const byte LEDS   = 4;
-const byte LEVEL1  = 6;
+const byte LEVEL1 = 6;
+const byte LEVEL2 = 8;
+const byte LEVEL3 = 10;
 
 void waitKey(char key){
   switch(key){
@@ -41,18 +43,18 @@ void config() {
     EnableInterrupts;
 }
 
-void symonSequency(byte *seq) {
+void symonSequency(byte *seq, byte level) {
   byte led=0, i;
   
-  for(i=0;i<LEVEL1;i++) {
+  for(i=0;i<level;i++) {
     led = rand() % LEDS + 1;
     seq[i] = led;
   }
 }
 
-void showGame(byte *sequency) {
+void showGame(byte *sequency, byte level) {
   byte i;
-  for (i=0; i<LEVEL1; i++) {
+  for (i=0; i<level; i++) {
     switch(sequency[i]) {
       case 1:
         PTFD_PTFD0 = 1;
@@ -70,10 +72,10 @@ void showGame(byte *sequency) {
   }
 }
 
-byte play(byte *sequency) {
+byte play(byte *sequency, byte level) {
   byte i=0, stage=0;
   
-  for(i=0; i<LEVEL1; i++) {
+  for(i=0; i<level; i++) {
     waitKey(1);
     if (PTAD_PTAD4 == 1) {
       if (sequency[i] == 1)
@@ -93,22 +95,50 @@ byte play(byte *sequency) {
   return stage;
 }
 
-void main(void) {
-  byte *sequency = (byte*)malloc(LEVEL1 * sizeof(byte));
-  byte stage=0;
-      
-  LCDinit();
-  config();                   //Configuracion de leds y push button
-  symonSequency(sequency);    //Almaceno secuencia del juego
-  showGame(sequency);         //Mostrar secuencia al jugador
-  
-  stage = play(sequency);     //Validacion de secuencia por el jugador            
-    
-  if (stage == LEVEL1) {      
-    LCDString("Correct!");
+void evaluate(byte *sequency, byte *level, byte *stage, byte currentLevel){
+  showGame(sequency, *level);         //Mostrar secuencia al jugador
+  *stage = play(sequency, *level);     //Validacion de secuencia por el jugador            
+  if (*stage == currentLevel) {      
+    LCDString("WIN!");
+    *stage = 0;
+    *level++;
   } else {
     LCDString("YOU LOSE!");
-  }
+  }  
+}
+
+void main(void) {
+  byte *sequency = (byte*)malloc(LEVEL1 * sizeof(byte));
+  byte *sequency2 = (byte*)malloc(LEVEL2 * sizeof(byte));
+  byte *sequency3 = (byte*)malloc(LEVEL3 * sizeof(byte));
+  byte stage=0, gLevel=1;
+      
+  LCDinit();
+  config();                           //Configuracion de leds y push button
   
+  symonSequency(sequency,  LEVEL1);    //Almaceno secuencia primer nivel
+  symonSequency(sequency2, LEVEL2);    //Almaceno secuencia segundo nivel
+  symonSequency(sequency3, LEVEL3);    //Almaceno secuencia tercer nivel
+  
+  while(gLevel <= 4) {
+    switch (gLevel) {
+      case 4:
+        LCDString("FINISH 3 STAGES");
+        gLevel++;
+        break;
+      case 1:
+        evaluate(sequency, &gLevel, &stage, LEVEL1);  
+        break;
+      case 2:
+        evaluate(sequency2, &gLevel, &stage, LEVEL2);
+        break;
+      case 3:
+        evaluate(sequency3, &gLevel, &stage, LEVEL3);
+        break;
+    }  
+  }
+    
   free(sequency);
+  free(sequency2);
+  free(sequency3);
 }
